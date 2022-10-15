7ff803f7869124d0170555cbdc6fca0f96d50130
elpif13 <113675207+elpif13@users.noreply.github.com>
2022-10-15 22:10:41 +0300

    Create Kategori.java

---
+++ Kategori.java
@@ -0,0 +1,26 @@
+package oopWithKodlamaioApp.entities;
+
+import java.util.ArrayList;
+
+public class Kategori {
+	
+	 private String name;
+	 
+	 public Kategori(String name) {
+		 this.name=name;
+		 
+	 }
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	 
+	
+}
