3ff276fccb8b8e8e6e04acb023e30396fdae4865
elpif13 <113675207+elpif13@users.noreply.github.com>
2022-10-15 22:09:32 +0300

    Create Eğitmen.java

---
+++ Eğitmen.java
@@ -0,0 +1,27 @@
+package oopWithKodlamaioApp.entities;
+
+public class Eğitmen {
+	
+	private String name;
+	
+	public Eğitmen(String name) {
+		
+		
+		this.name=name;
+		
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+
+}
